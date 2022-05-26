5dab6b11c4f4915fa85c2a340e12f6c86f26d362
Александр Семенов <korwin86@gmail.com>
2022-05-26 16:12:09 +0700

    тест sqllite

---
+++ AT/Data/SqiteDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT.Data
+{
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Device>? Devices { get; set; }
+        public DbSet<Software>? Softwares { get; set; }
+        public DbSet<Сomputer>? Сomputers { get; set; }
+        public DbSet<OS>? OperatingSystems{ get; set; }
+        public DbSet<PackageManager>? PackageManagers{ get; set; }
+        public DbSet<Shell>? Shells{ get; set; }
+
+        public SqliteDbContext()
+        {
+
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source=.\\data.db");
+    }
+}
---
+++ AT/Migrations/20220526081811_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AT.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Softwares",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", nullable: false),
+                    PackageManagerId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ShellId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Softwares", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Softwares_Softwares_PackageManagerId",
+                        column: x => x.PackageManagerId,
+                        principalTable: "Softwares",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Softwares_Softwares_ShellId",
+                        column: x => x.ShellId,
+                        principalTable: "Softwares",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IPAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", nullable: false),
+                    OperatingSystemId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Softwares_OperatingSystemId",
+                        column: x => x.OperatingSystemId,
+                        principalTable: "Softwares",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_OperatingSystemId",
+                table: "Devices",
+                column: "OperatingSystemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Softwares_PackageManagerId",
+                table: "Softwares",
+                column: "PackageManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Softwares_ShellId",
+                table: "Softwares",
+                column: "ShellId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Softwares");
+        }
+    }
+}
